6de25b017e2ccbaf6c25c88a420ecdc53e1bf7a2
Jesse Stuart <jesse@jessestuart.ca>
2017-07-05 12:11:45 -0400

    Use pool of reusable runtimes to speed things up
    Before:
    
    BenchmarkTransformString-8                    	       2 786798532 ns/op	132809604 B/op	 3456847 allocs/op
    BenchmarkTransformStringWithSingletonPool-8   	       2 769548317 ns/op	132805856 B/op	 3456834 allocs/op
    BenchmarkTransformStringWithLargePool-8       	       2 871457799 ns/op	132803644 B/op	 3456831 allocs/op
    
    After:
    
    BenchmarkTransformString-8                    	     300   5612375 ns/op	  925384 B/op	   15779 allocs/op
    BenchmarkTransformStringWithSingletonPool-8   	     300   5643058 ns/op	  926743 B/op	   15800 allocs/op
    BenchmarkTransformStringWithLargePool-8       	     300   5670713 ns/op	  926626 B/op	   15799 allocs/op
    
    Closes #1
    
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- babel.go
+++ babel.go
@@ -5,9 +5,41 @@ import (
 	"io"
 	"io/ioutil"
 	"strings"
+	"sync"
 
 	"github.com/dop251/goja"
 )
+
+const DefaultPoolSize = 1
+
+type babelTransformer struct {
+	Runtime   *goja.Runtime
+	Transform func(string, map[string]interface{}) (goja.Value, error)
+	Ready     chan struct{}
+}
+
+func (t *babelTransformer) Done() {
+	t.Ready <- struct{}{}
+}
+
+var mux sync.RWMutex
+var pool []*babelTransformer
+
+func Init(poolSize int) error {
+	mux.Lock()
+	defer mux.Unlock()
+	pool = make([]*babelTransformer, poolSize)
+	for i := 0; i < poolSize; i++ {
+		vm := goja.New()
+		transformFn, err := loadBabel(vm)
+		if err != nil {
+			return err
+		}
+		pool[i] = &babelTransformer{Runtime: vm, Transform: transformFn, Ready: make(chan struct{}, 1)}
+		pool[i].Ready <- struct{}{} // Transformer available for use
+	}
+	return nil
+}
 
 func Transform(src io.Reader, opts map[string]interface{}) (io.Reader, error) {
 	data, err := ioutil.ReadAll(src)
@@ -25,16 +57,38 @@ func TransformString(src string, opts map[string]interface{}) (string, error) {
 	if opts == nil {
 		opts = map[string]interface{}{}
 	}
-	vm := goja.New()
-	transform, err := loadBabel(vm)
+	t, err := getTransformer()
 	if err != nil {
 		return "", err
 	}
-	v, err := transform(src, opts)
+	defer func() { t.Done() }() // Make transformer available again when we're done
+	v, err := t.Transform(src, opts)
 	if err != nil {
 		return "", err
 	}
-	return v.ToObject(vm).Get("code").Export().(string), nil
+	vm := t.Runtime
+	return v.ToObject(vm).Get("code").String(), nil
+}
+
+func getTransformer() (*babelTransformer, error) {
+	// Make sure we have a pool created
+	if len(pool) == 0 {
+		if err := Init(DefaultPoolSize); err != nil {
+			return nil, err
+		}
+	}
+	mux.RLock()
+	defer mux.RUnlock()
+	for {
+		// find first available transformer
+		for _, t := range pool {
+			select {
+			case <-t.Ready:
+				return t, nil
+			default:
+			}
+		}
+	}
 }
 
 func loadBabel(vm *goja.Runtime) (func(string, map[string]interface{}) (goja.Value, error), error) {
