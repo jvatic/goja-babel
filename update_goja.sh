9d83e8648d255c8bdf78d7d0e835d8c5d420d04b
Jesse Stuart <email@jessestuart.ca>
2021-05-31 15:03:38 -0400

    update_goja: handle no available update

--- update_goja.sh
+++ update_goja.sh
@@ -6,6 +6,11 @@ GOJA_REPO="github.com/dop251/goja"
 CURRENT_VERSION=$(go mod edit --json | jq -r ".Require[] | select(.Path == \"$GOJA_REPO\") | .Version | .")
 LATEST_VERSION=$(go list --json -u -m $GOJA_REPO | jq -r ".Update.Version")
 
+if [ "$LATEST_VERSION" == "null" ]
+then
+    LATEST_VERSION=$CURRENT_VERSION
+fi
+
 echo "CURRENT_VERSION=$CURRENT_VERSION" >> $GITHUB_ENV
 echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
 
