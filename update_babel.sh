fcb0a09942c4ad0cc1409ad67a323278557e58f9
Jesse Stuart <email@jessestuart.ca>
2026-09-18 12:41:33 -0400

    fix(babel-updater): don't attempt 7.x versions
    for some reason these are still getting tagged as @latest

--- update_babel.sh
+++ update_babel.sh
@@ -2,7 +2,7 @@
 
 set -euox pipefail
 
-LATEST_VERSION=$(curl -i https://unpkg.com/@babel/standalone | grep -i Location | grep -Eo '[0-9][.0-9]+[0-9]')
+LATEST_VERSION=$(curl -i "https://unpkg.com/@babel/standalone@>=8" | grep -i Location | grep -Eo '[0-9][.0-9]+[0-9]')
 CURRENT_VERSION=$(cat babel_version.txt)
 
 echo "CURRENT_VERSION=$CURRENT_VERSION" >> $GITHUB_ENV
